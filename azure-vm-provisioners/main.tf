ce6d07f0d7cd5be536e0952407c9c2f93b75b149
Alisson <alissoncorrealima@hotmail.com>
2025-07-31 00:48:58 -0300

    ok

---
+++ azure-vm-provisioners/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_version = ">= 1.12.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.35.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "alissonterraformstate"
+    container_name       = "remote-state"
+    key                  = "azure-vm/terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "terraform_remote_state" "vnet" {
+  backend = "azurerm"
+  config = {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "alissonterraformstate"
+    container_name       = "remote-state"
+    key                  = "azure-vnet/terraform.tfstate"
+  }
+}
---
+++ azure-vm-provisioners/variables.tf
@@ -0,0 +1,6 @@
+variable "location" {
+  description = "Região onde os recursos da Azure serão criados"
+  type        = string
+  default     = "West Europe"
+}
+
